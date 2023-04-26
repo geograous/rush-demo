faf17b325895a9a3a086718910dadbee73900643
zhuhao <zhuhao@shizhuang-inc.com>
2023-04-26 15:36:39 +0800

    chore: 添加项目

--- apps/app1/src/views/AboutView.vue
+++ apps/app1/src/views/AboutView.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="inc">The button above has been clicked {{ count }} times.</button>
   </div>
 </template>
+
+<script lant="ts">
+// yarn 只能够使用第三方包，不支持 workspace
+// import {inc} from 'inc'
+export default {
+  data() {
+	  return {
+    	count: 0
+  	}
+	},
+  methods: {
+    inc() {
+      this.count = this.count+1
+    }
+  }
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
---
+++ libs/inc/index.js
@@ -0,0 +1,8 @@
+const add = (a, b) => a + b;
+const inc = (a) => a + 1;
+
+module.exports = {
+  add,
+  inc
+}
+
