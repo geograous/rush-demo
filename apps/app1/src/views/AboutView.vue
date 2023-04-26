8595f8f68aeb114fa453fbd041d525191c4fba95
zhuhao <zhuhao@shizhuang-inc.com>
2023-04-26 15:43:23 +0800

    style: 修改

--- apps/app1/src/views/AboutView.vue
+++ apps/app1/src/views/AboutView.vue
@@ -1,25 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <button @click="inc">The button above has been clicked {{ count }} times.</button>
+    <div>
+      <h1>This is an about page</h1>
+      <button @click="inc">The button above has been clicked {{ count }} times.</button>
+    </div>
   </div>
 </template>
 
-<script lant="ts">
+<script setup lang="ts">
+import {ref} from 'vue'
 // yarn 只能够使用第三方包，不支持 workspace
 // import {inc} from 'inc'
-export default {
-  data() {
-	  return {
-    	count: 0
-  	}
-	},
-  methods: {
-    inc() {
-      this.count = this.count+1
-    }
-  }
+
+const count = ref(0)
+
+const inc = () => {
+  count.value = count.value+1
 }
+
 </script>
 
 <style>
